374909cbba99b917a281b62e007d9c8400b77e65
electrolyteJ <hawks.jamesf@gmail.com>
2023-03-30 23:09:46 +0800

    u

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,40 +1,105 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
-
-import "taro-ui/dist/style/components/button.scss" // 按需引入
-import './index.scss'
-// import { ChatGPTAPI } from 'chatgpt'
-
-async function talk() {
-  console.log("talk")
-  // const apiKey = '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'
-  // const api = new ChatGPTAPI({
-  //   apiKey: apiKey
-  // })
-
-  // const res = await api.sendMessage('Hello World!')
-  // console.log(res.text)
+import React, { Component } from "react";
+import { View, Text, Input, Image } from "@tarojs/components";
+import { AtInput, AtForm, AtList, AtListItem, AtCard } from "taro-ui";
+import Taro from "@tarojs/taro";
+import "./index.scss";
+import VirtualList from "@tarojs/components/virtual-list";
+async function talk(txt) {
+  const res = await Taro.request({
+    url: `https://chatbot-server-production-cf19.up.railway.app/api/chat/txt?content=${txt}`,
+    method: "POST",
+  });
+  console.log(res);
+  return res.data.content;
 }
+function buildData(offset = 0) {
+  return Array(100)
+    .fill(0)
+    .map((_, i) => i + offset);
+}
+const Row = React.memo(({ id, index, data }) => {
+  return (
+    <AtCard
+      className={index % 2 ? "ListItemOdd" : "ListItemEven"}
+      id={id}
+      note="小Tips"
+      extra="额外信息"
+      title="这是个标题"
+      thumb="http://www.logoquan.com/upload/list/20180421/logoquan15259400209.PNG"
+    >
+      这也是内容区 可以随意定义功能 Row {index} : {data[index]}
+    </AtCard>
+    // <View
+    //   id={id}
+    //   className={index % 2 ? "ListItemOdd" : "ListItemEven"}
+    //   style="background:red"
+    // >
+    //   Row {index} : {data[index]}
+    // </View>
+  );
+});
+
 export default class Index extends Component {
-  componentDidMount () { }
+  state = {
+    data: buildData(0),
+    txt: "hi",
+  };
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
+  handleChange(value) {
+    this.setState({
+      value,
+    });
+    // 在小程序中，如果想改变 value 的值，需要 `return value` 从而改变输入框的当前值
+    return value;
+  }
 
-  render () {
+  render() {
+    const { data } = this.state;
+    const dataLen = data.length;
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true} onClick={()=>talk()}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+      <View className="flex-wrp" style="height:100%">
+        <View>
+          {/* <Text>{this.state.txt}</Text> */}
+          <Text style="background:red">ChatGPT</Text>
+          <VirtualList
+            height={1800} /* 列表的高度 */
+            width="100%" /* 列表的宽度 */
+            item={Row} /* 列表单项组件，这里只能传入一个组件 */
+            itemData={data} /* 渲染列表的数据 */
+            itemCount={dataLen} /* 渲染列表的长度 */
+            itemSize={100} /* 列表单项的高度  */
+          />
+        </View>
+        <AtInput
+          confirmType="send"
+          cursor
+          adjustPosition
+          // autoFocus
+          // focus
+          border={true}
+          name="value"
+          // title="标准五个字"
+          type="text"
+          placeholder="输入问题"
+          value={this.state.value}
+          onChange={this.handleChange.bind(this)}
+          onConfirm={async () => {
+            const t = await talk("有没有兴趣出去玩");
+            this.setState({ txt: t });
+          }}
+        >
+          {/* <Image src="https://aotu.io/img.png" /> */}
+        </AtInput>
       </View>
-    )
+    );
   }
 }
